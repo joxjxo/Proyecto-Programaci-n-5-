e423afdf34cc26f640422c74e585a5ced83a5d63
joxjxo <joxx2011@gmail.com>
2017-05-28 15:39:34 -0600

    primero

---
+++ Cotizador/Cotizador.UI/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cotizador.UI.Startup))]
+namespace Cotizador.UI
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
